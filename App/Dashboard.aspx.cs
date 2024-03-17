c27b56f951346887a9586ebcabea251b87164d11
Kiwi <allanvillegas35@gmail.com>
2024-03-17 19:42:55 +0800

    page: added user dashboard page

---
+++ App/Dashboard.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Flexify_App.App
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["LoggedIn"] == null || !(bool)Session["LoggedIn"])
+            {
+                Response.Redirect("~/Login.aspx");
+                return;
+            }
+        }
+    }
+}
